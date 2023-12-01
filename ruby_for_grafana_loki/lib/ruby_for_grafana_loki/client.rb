52256df85a0695972da09638314b29a4181a4f68
Oleg Ten <tennet0505@gmail.com>
2023-12-01 18:39:29 +0100

    change payload for rwquest

--- ruby_for_grafana_loki/lib/ruby_for_grafana_loki/client.rb
+++ ruby_for_grafana_loki/lib/ruby_for_grafana_loki/client.rb
@@ -1,24 +1,29 @@
 module RubyForGrafanaLoki class Client
- include RailsLokiExporterDev::Request
-
-        def send_log(log_message)
-          curr_datetime = Time.now.to_i * 1_000_000_000
-          labels = "{source=\"my_gem\",job=\"my_gem_job\"}"
-
-          payload = {
-            "streams" => [
-             {
-               "stream" => { "foo" => "bar2" },
-               "values" => [
-                ["#{curr_datetime}", log_message]
-                ]
+  include RailsLokiExporterDev::Request
+    def send_log(log_message)
+      curr_datetime = Time.now.to_i * 1_000_000_000
+      payload = {
+        "streams" => [
+          {
+            "stream" => {
+            "foo" => "bar2",
+            "source" => "Name-of-your-source",
+            "job" => "name-of-your-job",
+            "host" => host
+             },
+            "values" => [["#{curr_datetime}", log_message]],
+            "entries" => [
+              {
+                "ts" => curr_datetime,
+                "line" => "[WARN] " + msg
               }
             ]
           }
-
-          json_payload = JSON.generate(payload)
-          uri = '/loki/api/v1/push'
-          post uri, json_payload
-        end
+        ]
+      }
+      json_payload = JSON.generate(payload)
+      uri = '/loki/api/v1/push'
+      post uri, json_payload
     end
+  end
 end
