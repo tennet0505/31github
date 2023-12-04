ed85a5934b5c1c4f8ac633fb3e8b1b2719e2219a
Oleg Ten <tennet0505@gmail.com>
2023-12-04 15:47:59 +0100

    update README

--- ruby_for_grafana_loki/lib/ruby_for_grafana_loki/client.rb
+++ ruby_for_grafana_loki/lib/ruby_for_grafana_loki/client.rb
@@ -48,7 +48,7 @@ end
 #
 # # Example with Rails logger
 # rails_logger = Logger.new(STDOUT)
-# client = RailsLokiExporterDev.client(rails_logger)
+# client = RubyForGrafanaLoki.client(rails_logger)
 #
 # # Send a log entry to Loki
 # client.send_log("This is a log message.")
