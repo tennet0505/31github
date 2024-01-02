ceb00fdabc75bcafb5111d947f01eedb10134cbc
Oleg Ten <tennet0505@gmail.com>
2024-01-02 17:27:01 +0100

    update

--- ruby_for_grafana_loki/lib/ruby_for_grafana_loki/client.rb
+++ ruby_for_grafana_loki/lib/ruby_for_grafana_loki/client.rb
@@ -1,6 +1,8 @@
 # lib/ruby_for_grafana_loki.rb
 
 module RubyForGrafanaLoki
+  LOGS_TYPE = %w(ERROR WARN FATAL INFO DEBUG).freeze
+
   class Client
     include RubyForGrafanaLoki::Connection
     # LOGS_TYPE = %w(ERROR WARN FATAL INFO DEBUG).freeze
@@ -22,7 +24,7 @@ module RubyForGrafanaLoki
       @log_buffer = []
       @last_interaction_time = nil
       @interaction_interval = 1 # in seconds, adjust as needed
-      @max_buffer_size = 20 # set the maximum number of logs to buffer
+      @max_buffer_size = 10 # set the maximum number of logs to buffer
       @logger = InterceptingLogger.new(intercept_logs: @intercept_logs)
       @logger.client = self
       @connection = connection
@@ -43,7 +45,7 @@ module RubyForGrafanaLoki
           send_buffered_logs
           @last_interaction_time = Time.now
         else
-          @logger.info('Log buffered. Waiting for more logs or interaction interval.')
+          # @logger.info('Log buffered. Waiting for more logs or interaction interval.')
         end
       end
     end
@@ -88,10 +90,11 @@ module RubyForGrafanaLoki
     end
 
     def match_logs_type?(log_line)
-      type = log_line.match(/(ERROR|WARN|FATAL|INFO|DEBUG)/)&.to_s
+      return false if log_line.nil?
+
+      type_match = log_line.match(/(ERROR|WARN|FATAL|INFO|DEBUG)/)
+      type = type_match&.to_s
       @logs_type.include?(type)
     end
   end
 end
-
-
--- ruby_for_grafana_loki/lib/ruby_for_grafana_loki/intercepting_logger.rb
+++ ruby_for_grafana_loki/lib/ruby_for_grafana_loki/intercepting_logger.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 require 'json'
 require 'net/http'
+
 module RubyForGrafanaLoki
   class InterceptingLogger < Logger
     attr_accessor :client
 
+    SEVERITY_NAMES = %w(DEBUG INFO WARN ERROR FATAL).freeze
+
     def initialize(intercept_logs: false)
       @intercept_logs = intercept_logs
       @log = ""
-
-      # Set a formatter, you can customize it as needed
-      self.formatter = proc { |severity, datetime, _progname, msg|
-        "#{datetime.strftime('%Y-%m-%d %H:%M:%S')} [#{severity}] #{msg}\n"
-        @log = "[#{severity}] #{msg}"
-      }
       self.level = Logger::DEBUG
     end
 
     def add(severity, message = nil, progname = nil, &block)
+
+      severity_name = severity_name(severity)
+      log_message = message || (block&.call)
+
+      @log = format_message(severity_name, Time.now, progname, log_message)
+
       if @intercept_logs
         client.send_log(@log) if client
       end
@@ -28,5 +31,14 @@ module RubyForGrafanaLoki
       client.send_log(@log) if client
     end
 
+    private
+
+    def format_message(severity, datetime, progname, msg)
+      "#{severity} #{progname}: #{msg}\n"
+    end
+
+    def severity_name(severity)
+      SEVERITY_NAMES[severity] || "UNKNOWN"
+    end
   end
 end
